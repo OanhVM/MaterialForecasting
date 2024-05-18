aeb1bbb1e7a2046b98153806ccb374707596c97c
OanhVM <vumaioanh89@gmail.com>
2024-05-18 16:07:48 +0300

    Updated `lstm.train_and_eval_lstm()` according to recent changes + Added data balancing

--- src/models/lstm.py
+++ src/models/lstm.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from random import Random
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 import numpy as np
 import tensorflow as tf
@@ -12,34 +12,55 @@ from keras.src.optimizers import Adam
 from numpy import ndarray
 
 
+def get_balanced_inputs_and_labels(
+        input_label_pairs_per_input_width: Dict[int, List[Tuple[ndarray, ndarray]]],
+) -> Dict[int, List[Tuple[ndarray, ndarray]]]:
+    bin_sizes = np.asarray(input_label_pairs_per_input_width.keys())
+
+    balanced_coeffs = bin_sizes ** 0.25
+    balanced_coeffs /= balanced_coeffs.sum()
+
+    balanced_bin_sizes = np.round(balanced_coeffs * bin_sizes.sum()).astype(int)
+
+    balanced_input_label_pairs_per_input_width = {
+        width: Random(37).sample(cont_seqs, k=balanced_bin_sizes[idx])
+        for idx, (width, cont_seqs) in enumerate(input_label_pairs_per_input_width.items())
+    }
+
+    return balanced_input_label_pairs_per_input_width
+
+
 def get_datasets(
-        cont_seqs: List[ndarray], out_n_step: int, val_ratio: float = 0.2,
+        inputs: List[ndarray], labels: List[ndarray], do_balance: bool,
+        val_ratio: float = 0.2,
 ) -> Tuple[
     tf.data.Dataset, tf.data.Dataset,
 ]:
-    def _make_dataset(__cont_seqs: List[ndarray]) -> tf.data.Dataset:
-        assert len(__cont_seqs) > 0
+    def _make_dataset(_input_label_pairs: List[Tuple[ndarray, ndarray]]) -> tf.data.Dataset:
+        return tf.data.Dataset.from_tensors(
+            _input_label_pairs
+        ).shuffle(
+            buffer_size=len(_input_label_pairs),
+        ).batch(
+            batch_size=len(_input_label_pairs),
+        ).cache()
 
-        __cont_seqs = np.asarray(__cont_seqs).astype(np.float32)[..., np.newaxis]
+    input_label_pairs_per_input_width = defaultdict(list)
+    for _input, label in zip(inputs, labels):
+        input_label_pairs_per_input_width[len(_input)].append((_input, label))
 
-        inputs = __cont_seqs[:, :-out_n_step, :]
-        labels = __cont_seqs[:, -out_n_step:, :]
-
-        return tf.data.Dataset.from_tensor_slices((inputs, labels)).batch(len(__cont_seqs))
-
-    Random(42).shuffle(cont_seqs)
-
-    cont_seqs_per_width = defaultdict(list)
-    for cont_seq in cont_seqs:
-        cont_seqs_per_width[len(cont_seq)].append(cont_seq)
+    if do_balance:
+        input_label_pairs_per_input_width = get_balanced_inputs_and_labels(
+            input_label_pairs_per_input_width=input_label_pairs_per_input_width,
+        )
 
     train_ds, val_ds = None, None
-    for _cont_seqs in cont_seqs_per_width.values():
-        if len(_cont_seqs) > 1:
-            val_size = max(int(round(len(_cont_seqs) * val_ratio)), 1)
+    for input_label_pairs in input_label_pairs_per_input_width.values():
+        if len(input_label_pairs) > 1:
+            val_size = max(int(round(len(input_label_pairs) * val_ratio)), 1)
 
-            _train_ds = _make_dataset(_cont_seqs[:-val_size])
-            _val_ds = _make_dataset(_cont_seqs[-val_size:])
+            _train_ds = _make_dataset(input_label_pairs[:-val_size])
+            _val_ds = _make_dataset(input_label_pairs[-val_size:])
 
             train_ds = _train_ds if train_ds is None else train_ds.concatenate(_train_ds)
             val_ds = _val_ds if val_ds is None else val_ds.concatenate(_val_ds)
@@ -47,40 +68,15 @@ def get_datasets(
     return train_ds, val_ds
 
 
-def eval_model(
-        model: Model, train_ds: tf.data.Dataset, val_ds: tf.data.Dataset, horizons: List[int],
-) -> Tuple[
-    List[float], Tuple[tf.data.Dataset, tf.data.Dataset],
-]:
-    labels, preds = [], []
-    for _inputs, _labels in train_ds.concatenate(val_ds):
-        _inputs = tf.convert_to_tensor(_inputs)
-        _labels = tf.convert_to_tensor(_labels)
-        _preds = model(_inputs)
-
-        labels.extend(_labels)
-        preds.extend(_preds)
-
-    labels = tf.convert_to_tensor(labels)
-    preds = tf.convert_to_tensor(preds)
-
-    squared_errs = (labels - preds) ** 2
-    rmses = [
-        float(tf.reduce_mean(squared_errs[:, :horizon]) ** 0.5)
-        for horizon in horizons
-    ]
-    return rmses, (labels, preds)
-
-
 def build_and_train_lstm(
         train_ds: tf.data.Dataset, val_ds: tf.data.Dataset,
-        n_neuron: int, out_n_step: int, n_epoch: int,
+        n_neuron: int, label_width: int, n_epoch: int,
         loss_name: str = "mean_squared_error", learning_rate: float = 0.5e-3,
 ) -> Model:
     model = Sequential([
         LSTM(n_neuron),
-        Dense(out_n_step, kernel_initializer=tf.initializers.zeros()),
-        Reshape([out_n_step, 1]),
+        Dense(label_width, kernel_initializer=tf.initializers.zeros()),
+        Reshape([label_width, 1]),
     ])
     model.compile(loss=loss_name, optimizer=Adam(learning_rate=learning_rate), metrics=[RootMeanSquaredError()])
 
@@ -94,20 +90,20 @@ def build_and_train_lstm(
 
 
 def train_and_eval_lstm(
-        cont_seqs: List[ndarray], horizons: List[int],
-        n_neuron: int, n_epoch: int = 200,
-) -> List[float]:
-    out_n_step = max(horizons)
-
-    # TODO: make optionally rebalanced
-    train_ds, val_ds = get_datasets(cont_seqs=cont_seqs, out_n_step=out_n_step)
+        inputs: List[ndarray], labels: List[ndarray], label_width: int,
+        n_neuron: int, do_balance: bool,
+        n_epoch: int = 200,
+) -> List[ndarray]:
+    train_ds, val_ds = get_datasets(inputs=inputs, labels=labels, do_balance=do_balance)
 
     # TODO: make federated
     model = build_and_train_lstm(
         train_ds=train_ds, val_ds=val_ds,
-        n_neuron=n_neuron, out_n_step=out_n_step, n_epoch=n_epoch,
+        n_neuron=n_neuron, label_width=label_width, n_epoch=n_epoch,
     )
 
-    rmses, _ = eval_model(model=model, train_ds=train_ds, val_ds=val_ds, horizons=horizons)
+    preds = []
+    for _inputs, _ in train_ds.concatenate(val_ds):
+        preds.extend(model.predict(_inputs))
 
-    return rmses
+    return preds
